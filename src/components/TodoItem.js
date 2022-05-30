ce0ff87b3c256824da3e025d07a4bb4bdb754526
Deborah <dereyes15@gmail.com>
2022-05-30 13:38:23 -0700

    add TodoItem styles

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,23 @@
 import React from "react"
 
+//styles
+import styles from "./TodoItem.module.css"
+
 class TodoItem extends React.Component {
 	render() {
 		return(
-			<li> 
+			<li className={styles.item}> 
 				<input 
+					className={styles.checkbox}
 					type="checkbox" 
 					checked={this.props.todo.completed}
 					onChange={() => this.props.handleChangeProps(this.props.todo.id)} 
 				/>
-				<button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}> Delete </button>
+				<button 
+				onClick={() => this.props.deleteTodoProps(this.props.todo.id)}
+				> 
+					Delete 
+				</button>
 				{this.props.todo.title} 
 			</li>
 		)
