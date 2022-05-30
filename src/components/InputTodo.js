929ca78563b28a8476071070b68d4ffd649e1702
Deborah <dereyes15@gmail.com>
2022-05-30 12:52:58 -0700

    Create Input Todo component

---
+++ src/components/InputTodo.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react"
+
+class InputTodo extends Component {
+	state = {
+		title: ""
+	}
+
+	onChange = e => {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+		console.log(this.state.title);
+	};
+
+	render() {
+		return(
+			<form onSubmit={this.handleSubmit}>
+				<input 
+					type="text" 
+					name="title"
+					placeholder="What's your next to-do? ..." 
+					value={this.state.title}
+					onChange={this.onChange}
+				/>
+				<button> Submit </button>
+			</form>
+		)
+	}
+}
+
+export default InputTodo
